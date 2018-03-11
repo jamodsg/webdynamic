28c193961a889de60272fd7578be0161feb0ae5b
jamodsg <jamodsg@gmail.com>
2018-03-11 12:12:44 -0700

    for testing

--- api/models/Carousel.js
+++ api/models/Carousel.js
@@ -11,7 +11,7 @@ module.exports = {
     relatedPost: {
       type: 'integer' // must be post id
     },
-    post: {
+    posts: {
       model: 'Post' // Post should be single <li>content the carousel image</li>
     }
   }
